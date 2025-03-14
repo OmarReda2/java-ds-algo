b07a3a64d6b3c0e35dc5c74e2fc028cfbaaedc27
Omar Reda <omarredaelsayied@gmail.com>
2025-03-14 14:27:18 +0200

    Queue - exc: Push for a Stack That Uses an ArrayList

--- DS-Algo-prac/src/datastructures/stack/impl/Stack.java
+++ DS-Algo-prac/src/datastructures/stack/impl/Stack.java
@@ -1,4 +1,4 @@
-package datastructures.stack;
+package datastructures.stack.impl;
 
 public class Stack {
 
---
+++ DS-Algo-prac/src/datastructures/stack/problem_1/Main.java
@@ -0,0 +1,25 @@
+package datastructures.stack.problem_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack myStack = new Stack();
+
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+
+        myStack.printStack();
+
+        /*
+            EXPECTED OUTPUT:
+			3
+			2
+			1
+
+        */
+
+    }
+
+}
