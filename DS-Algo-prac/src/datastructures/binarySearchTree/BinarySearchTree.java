b36c2a7da5ea6aac0b8d73614ba88001378755c1
Omar Reda <omarredaelsayied@gmail.com>
2025-03-19 03:23:32 +0200

    trees: constructor

---
+++ DS-Algo-prac/src/datastructures/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,15 @@
+package datastructures.binarySearchTree;
+
+// binary search tree
+public class BinarySearchTree {
+
+    Node root; // initialize root to be null "root = null"
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+//        private Node() { root = null;}  // redundant as we initialize root
+    }
+}
